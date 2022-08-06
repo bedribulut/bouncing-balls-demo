2626009b2c8c82318b934b999b30c7c3611ed978
bedribulut <96528707+bedribulut@users.noreply.github.com>
2022-08-06 12:19:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ class Ball {
 
 const balls = [];
 
-while (balls.length < 1596) {
+while (balls.length < 111) {
    const size = random(10,20);
    const ball = new Ball(
       // ball position always drawn at least one ball width
